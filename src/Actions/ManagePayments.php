b7c59b622ccfbaa237bbc54c3dd38aafcd14969b
Vaibhavraj Roham <vaibhavraj.developer@gmail.com>
2020-10-27 21:24:12 +0530

    Added api to delete the existing uncaptured authorization

--- src/Actions/ManagePayments.php
+++ src/Actions/ManagePayments.php
@@ -4,6 +4,7 @@ namespace Vrajroham\PhpKlarna\Actions;
 
 use Vrajroham\PhpKlarna\Resources\CustomerTokenFromAuthorization;
 use Vrajroham\PhpKlarna\Resources\OrderCreated;
+use Vrajroham\PhpKlarna\Resources\RespondSuccess;
 use Vrajroham\PhpKlarna\Resources\Session;
 
 trait ManagePayments
@@ -17,15 +18,22 @@ trait ManagePayments
 
     public function createCustomerToken(string $authorizationToken, $data)
     {
-        $customerToken = $this->post("payments/v1/authorizations/".$authorizationToken."/customer-token", $data);
+        $customerToken = $this->post('payments/v1/authorizations/'.$authorizationToken.'/customer-token', $data);
 
         return new CustomerTokenFromAuthorization($customerToken, $this);
     }
 
     public function createOrderFromAuthorizationToken(string $authorizationToken, $data)
     {
-        $order = $this->post("payments/v1/authorizations/".$authorizationToken."/order", $data);
+        $order = $this->post('payments/v1/authorizations/'.$authorizationToken.'/order', $data);
 
         return new OrderCreated($order, $this);
+    }
+
+    public function cancelAuthorization(string $authorizationToken, $data)
+    {
+        $response = $this->delete('payments/v1/authorizations/'.$authorizationToken, $data);
+
+        return new RespondSuccess($response, $this);
     }
 }
